cee116512adb5ae47092ea2c46e1f1070cc1f7b3
jbanega <jban3ga@gmail.com>
2022-02-27 21:02:12 -0400

    Viewsets and routers implemented

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,3 +1,5 @@
+from webbrowser import get
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from .models import Post
 
@@ -5,10 +7,10 @@ from .models import Post
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = (
-            'id',
-            'author',
-            'title',
-            'body',
-            'created_at',
-        )
+        fields = ('id', 'author', 'title', 'body', 'created_at',)
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ('id', 'username',)
--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,12 @@
+from posixpath import basename
 from django.urls import path
-from .views import PostList, PostDetail
+from rest_framework.routers import SimpleRouter
+from .views import PostViewSet, UserViewSet
 
 
-urlpatterns = [
-    path('', PostList.as_view()),
-    path('<int:pk>/', PostDetail.as_view()),
-]
+router = SimpleRouter()
+router.register('users', UserViewSet, basename='users')
+router.register('', PostViewSet, basename='posts')
+
+
+urlpatterns = router.urls
